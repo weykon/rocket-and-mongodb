5633ad1e59368d4535c9c3bd2476fb8cd21ca5bb
weykon <635883092@qq.com>
2022-09-27 17:35:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+[source](https://dev.to/hackmamba/build-a-rest-api-with-rust-and-mongodb-rocket-version-ah5)
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod api;
+mod models;
+mod repository;
+
+#[macro_use]
+extern crate rocket;
+use rocket::{get, http::Status, serde::json::Json};
+
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from rust and mongoDB")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
+}
